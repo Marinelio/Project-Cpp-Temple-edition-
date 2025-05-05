c3de5e81f3c4bc377667dc0e7a9d80e503adbf28
Marinelio <marineliohidri@gmail.com>
2025-05-05 13:28:02 +0300

    just better

--- main.cpp
+++ main.cpp
@@ -1,344 +1,284 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <sstream>
+#include <string>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <iomanip>
+#include <cstring>
+#include <limits>
 
 using namespace std;
 
-
-
-class Password{
-	private:
-		string password;
-		string passwordType;
-	public:
-		string getPasswordType(){
-		return passwordType;
-		}
-		string getPassword(){
-		return password;
-		}
-		void setPasswordType(string tempPasswordType){
-			passwordType=tempPasswordType;
-		}
-		void setPassword(string tempPassword){
-			password=tempPassword;
-		}
-	
+// Structure to store password information
+struct Password {
+    char passwordType[50];  // Type/name (e.g., "Gmail", "Facebook")
+    char password[100];     // The encrypted password in hex
 };
 
-int serialSearch(Password passwords[10000],string passwordType,int passwordCount){
-	int i=0;
-	while(i<passwordCount){
-		if(passwords[i].getPasswordType()==passwordType){
-		return i;
-	}
-		i++;
-	}
-	return -1;
-	}
-	
-class FileManager{
-	private:
-		string fileName="passwords.txt";
-		
-	public:
-		
-	string openFile(){
-		string line;
-		
-		ifstream file ("passwords.txt");
-		
-		if(file.is_open()){
-			cout<<"file Open\n";
-			
-			stringstream buffer; //it's a string that u can add new things,but it doesn't read like string
-			buffer << file.rdbuf();//read the whole text and returns it like buffer
-			file.close();
-			return buffer.str();
-		
-		}else{
-			cout<<"creating password file";
-			ofstream file;
-			file.open("passwords.txt");
-			file.close();
-		}
-		
-			
-		}
-		
-		string getFileName(){
-			return fileName;
-		}
-		
-		void saveToFile(Password passwords[10000],int passwordCount){
-			
-			ofstream file("passwords.txt");
-			string line;
-			
-			if(file.is_open()){
-				for(int i=0;i<passwordCount;i++){
-					line=passwords[i].getPasswordType();
-					line=line + ":";
-					line=line + passwords[i].getPassword()+"\n";
-					file<<line;
-					
-				}
-				file.close();
-			}else{
-				cout<<"Unable to open file\n";
-			}
-			
-		}
-			
-	
-};
-class PasswordManager{
-	private:
-		Password passwords[10000];
-		FileManager fileManager;
-		int passwordCount;
-		
-	public:
-		PasswordManager (){
-			passwordCount=-1;
-		}
-		//ftiaxtei ta passwords/ apo string parse tis grammes se type Password
-		void parsePasswords(){
-			if(passwordCount!=-1){
-				return;//to count einai-1 otan den tous exei trabijei pote tous kodikous/gia na min ta janatrabaei
-			}
-			string line;
-			string passwordType;
-			string password;
-			
-			string fileText=fileManager.openFile();
-			stringstream stream(fileText);
-			passwordCount=0;
-			while(getline(stream,line)){//with this getline we get the whole line
-				int pos=line.find(":");
-				passwordType=line.substr(0,pos);
-				password=line.substr(pos+1);
-				
-				
-				passwords[passwordCount].setPasswordType(passwordType);
-				passwords[passwordCount].setPassword(password);
-				passwordCount++;
-				
-			
-			}			
-			
-			
-		}
-		void createNewPassword(){
-			string passwordType;
-			string password;
-			
-			parsePasswords(); //gia na fortothoun oi kodikoi
-			
-			cout<<"Password Type: ";
-			cin>>passwordType;
-			passwords[passwordCount].setPasswordType(passwordType);
-			cout<<"Password: ";
-			cin>>password;
-			passwords[passwordCount].setPassword(password);
-			
-			passwordCount++;
-			
-			
-			fileManager.saveToFile(passwords,passwordCount);
-			cout<<"Password created sucessfully\n";
-			
-				}
-				
-		void viewPasswords(){
-			parsePasswords();
-			cout<<passwordCount<<" Total passwords:\n";
-			for(int i=0 ;i<passwordCount;i++){
-				cout<<"password number "<<i<<endl;
-				cout<<passwords[i].getPasswordType()<<endl;
-				cout<<passwords[i].getPassword()<<endl;
-				cout<<"------------------------------------"<<endl;
-			}	
-		}
-		
-		void generatePassword(){
-			parsePasswords();
-			
-			string passwordType;
-			string password="";
-			int length;
-			string charset;
-			int charSize;
-			int randomNum;
-			
-			cout<<"Password Type: ";
-			cin>>passwordType;
-			passwords[passwordCount].setPasswordType(passwordType);
-			
-			cout<<"Enter the password length\n";
-			cin>>length;
-			
-			charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
-			srand(static_cast<unsigned int>(time(0)));
-			for(int i=0;i<length;i++){
-				charSize=charset.size();
-				randomNum= rand()%charSize;
-				password=password+charset[randomNum];
-			}
-			passwords[passwordCount].setPassword(password);
-			passwordCount++;
-		
-			fileManager.saveToFile(passwords,passwordCount);
-			cout<<"Password Generated sucessfully,new password:"<<password<<"\n";
-		}	
-		
-		void deletePassword(){
-			parsePasswords();
-			string passwordType;
-			int pos,i;
-			
-			if(passwordCount==0){
-				cout<<"there are no passwords\n";
-				return; 
-			}
-			viewPasswords();
-			cout<<"Enter the password you want to delete:\n";
-			cin>>passwordType;
-			
-			pos=serialSearch(passwords,passwordType,passwordCount);
-			
-			if(pos==-1){
-				cout<<"Invalid input\n";
-				return;
-			}
-			for(i=pos;i<passwordCount;i++){
-				passwords[i]=passwords[i+1];
-			}
-			passwordCount--;
-			viewPasswords();
-			
-			fileManager.saveToFile(passwords,passwordCount);
-			cout<<"Password deleted succesfully\n";
-			
-			
-			
-		}
-		
-		void editPassword(){
-			parsePasswords();
-			string passwordType;
-			int pos,choice;
-			string password;
-			string charset="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
-			
-			if(passwordCount==0){
-				cout<<"there are no passwords\n";
-				return; 
-			}
-			viewPasswords();
-			
-			cout<<"Enter the password you want to edit:\n";
-			cin>>passwordType;
-			
-			pos= serialSearch(passwords,passwordType,passwordCount);
-			
-			if(pos==-1){
-				cout<<"Invalid input\n";
-				return;
-			}
-			
-			cout<<"1.Custom input\n";
-			cout<<"2.Generate\n";
-			cout<<"3.Back\n";
-			cin>>choice;
-			switch(choice){
-				case 1:
-					cout<<"Password: ";
-					cin>>password;
-					passwords[pos].setPassword(password);
-					fileManager.saveToFile(passwords,passwordCount);
-					break;
-				case 2:
-					password="";
-					int length;
-					
-					int charSize;
-					int randomNum;
-					cout<<"Enter the password length\n";
-					cin>>length;
-			
-					
-					srand(static_cast<unsigned int>(time(0)));
-					for(int i=0;i<length;i++){
-						charSize=charset.size();
-						randomNum= rand()%charSize;
-						password=password+charset[randomNum];
-						}
-					passwords[pos].setPassword(password);
-					fileManager.saveToFile(passwords,passwordCount);	
-					break;
-				case 3:
-					return;
-					break;	
-					
-			}
-		}
-	};
-	
-class Menu{
-	public:
-		void show(){
-			cout<<"PASSWORD MANAGER\n";
-			cout<<"---------------------\n";
-			cout<<"1.View Passwords\n";
-			cout<<"2.Add new Password\n";
-			cout<<"3.Generate new Password\n";
-			cout<<"4.Edit Password\n";
-			cout<<"5.Delete Password\n";
-			cout<<"6.Exit\n";
-		}
-			
-	};
+vector<Password> passwords;
+const string FILENAME = "passwords.bin";
 
-/*class Authentication{
-	function encrypt password with ceasar and the key will be the password
-	function for decrypt
-	function for login(in the begining tha sigrine ton kodiko me hash, and idf the password was correct tha decrypt all the password -using the key as the password-)
-};*/
+// XOR key
+const string XOR_KEY = "Terry THE GOAT Davis";
+
+/**
+ * Read an integer choice between min and max (inclusive), reprompting on invalid input.
+ */
+int getChoice(int min, int max) {
+    int c;
+    while (true) {
+        if (cin >> c && c >= min && c <= max) {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return c;
+        }
+        cout << "Invalid choice. Please enter a number between " << min << " and " << max << ": ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+}
+
+/**
+ * XOR‐encrypts (and decrypts) a string, returning a hex-encoded result.
+ */
+string encryptPassword(const string& plain) {
+    stringstream ss;
+    ss << hex << setfill('0');
+    for (size_t i = 0; i < plain.size() && i < 49; ++i) {
+        unsigned char ec = plain[i] ^ XOR_KEY[i % XOR_KEY.size()];
+        ss << setw(2) << int(ec);
+    }
+    return ss.str();
+}
+
+/**
+ * Decodes a hex string and XOR‐decrypts it.
+ */
+string decryptPassword(const string& hexed) {
+    string result;
+    for (size_t i = 0; i + 1 < hexed.size(); i += 2) {
+        string byteStr = hexed.substr(i, 2);
+        unsigned char ec = static_cast<unsigned char>(stoi(byteStr, nullptr, 16));
+        char dc = ec ^ XOR_KEY[(i/2) % XOR_KEY.size()];
+        result.push_back(dc);
+    }
+    return result;
+}
+
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+int loadPasswords() {
+    ifstream file(FILENAME, ios::binary);
+    passwords.clear();
+    if (!file.is_open()) return 0;
+    Password temp;
+    while (file.read(reinterpret_cast<char*>(&temp), sizeof(temp))) {
+        passwords.push_back(temp);
+    }
+    file.close();
+    return passwords.size();
+}
+
+void savePasswords() {
+    ofstream file(FILENAME, ios::binary);
+    if (!file.is_open()) {
+        cout << "Unable to open file for saving.\n";
+        return;
+    }
+    for (auto& p : passwords) {
+        file.write(reinterpret_cast<const char*>(&p), sizeof(p));
+    }
+    file.close();
+}
+
+void viewPasswords() {
+    loadPasswords();
+    clearScreen();
+    cout << passwords.size() << " Total passwords:\n";
+    cout << "------------------------------------\n";
+    for (size_t i = 0; i < passwords.size(); ++i) {
+        string encryptedHex(passwords[i].password);
+        string plain = decryptPassword(encryptedHex);
+        cout << (i + 1) << ". "
+             << setw(15) << left << passwords[i].passwordType
+             << ": " << plain << "\n";
+    }
+    cout << "------------------------------------\n";
+}
+
+void addPassword() {
+    loadPasswords();
+    clearScreen();
+
+    cout << "Add New Password\n";
+    cout << "1. Custom input\n";
+    cout << "2. Generate random\n";
+    cout << "3. Back\n";
+    cout << "Choice: ";
+    int choice = getChoice(1, 3);
+    if (choice == 3) return;
+
+    Password newPass;
+    cout << "Password Type: ";
+    cin.getline(newPass.passwordType, sizeof(newPass.passwordType));
+
+    string plain;
+    if (choice == 1) {
+        cout << "Password (max 49 chars): ";
+        getline(cin, plain);
+        if (plain.size() > 49) {
+            cout << "Too long—truncating to 49 characters.\n";
+            plain.resize(49);
+        }
+    } else {
+        int length;
+        cout << "Desired length (max 49): ";
+        length = getChoice(1, 49);
+        const string charset =
+            "abcdefghijklmnopqrstuvwxyz"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "0123456789"
+            "!@#$%^&*()_-+=<>?";
+        srand(static_cast<unsigned>(time(nullptr)));
+        for (int i = 0; i < length; ++i) {
+            plain += charset[rand() % charset.size()];
+        }
+        cout << "Generated password: " << plain << "\n";
+    }
+
+    string hexed = encryptPassword(plain);
+    memset(newPass.password, 0, sizeof(newPass.password));
+    strncpy(newPass.password, hexed.c_str(), sizeof(newPass.password) - 1);
+
+    passwords.push_back(newPass);
+    savePasswords();
+    cout << "Password stored encrypted.\n";
+}
+
+int searchPassword(const string& type) {
+    for (size_t i = 0; i < passwords.size(); ++i) {
+        if (type == passwords[i].passwordType) return i;
+    }
+    return -1;
+}
+
+void deletePassword() {
+    loadPasswords();
+    if (passwords.empty()) {
+        clearScreen();
+        cout << "No passwords.\n";
+        return;
+    }
+    viewPasswords();
+    cout << "Type to delete: ";
+    string type;
+    getline(cin, type);
+
+    int idx = searchPassword(type);
+    if (idx >= 0) {
+        passwords.erase(passwords.begin() + idx);
+        savePasswords();
+        clearScreen();
+        cout << "Deleted.\n";
+    } else {
+        cout << "Not found.\n";
+    }
+}
+
+void editPassword() {
+    loadPasswords();
+    if (passwords.empty()) {
+        clearScreen();
+        cout << "No passwords.\n";
+        return;
+    }
+    viewPasswords();
+    cout << "Type to edit: ";
+    string type;
+    getline(cin, type);
+
+    int idx = searchPassword(type);
+    if (idx < 0) {
+        cout << "Not found.\n";
+        return;
+    }
+
+    clearScreen();
+    cout << "1. Custom input\n";
+    cout << "2. Generate random\n";
+    cout << "3. Back\n";
+    cout << "Choice: ";
+    int choice = getChoice(1, 3);
+    if (choice == 3) return;
+
+    string plain;
+    if (choice == 1) {
+        cout << "New password (max 49): ";
+        getline(cin, plain);
+        if (plain.size() > 49) plain.resize(49);
+    } else {
+        int length;
+        cout << "Desired length (max 49): ";
+        length = getChoice(1, 49);
+        const string charset =
+            "abcdefghijklmnopqrstuvwxyz"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "0123456789"
+            "!@#$%^&*()_-+=<>?";
+        srand(static_cast<unsigned>(time(nullptr)));
+        for (int i = 0; i < length; ++i) {
+            plain += charset[rand() % charset.size()];
+        }
+        cout << "Generated: " << plain << "\n";
+    }
+
+    string hexed = encryptPassword(plain);
+    memset(passwords[idx].password, 0, sizeof(passwords[idx].password));
+    strncpy(passwords[idx].password, hexed.c_str(), sizeof(passwords[idx].password) - 1);
+    savePasswords();
+
+    cout << "Password updated.\n";
+}
+
+void showMenu() {
+    clearScreen();
+    cout << "PASSWORD MANAGER\n"
+            "----------------\n"
+            "1. View Passwords\n"
+            "2. Add New Password\n"
+            "3. Generate Password\n"
+            "4. Edit Password\n"
+            "5. Delete Password\n"
+            "6. Exit\n"
+            "----------------\n"
+            "Enter your choice: ";
+}
+
 int main() {
-
-	int choice;
-	PasswordManager passwordManager;
-	Menu menu;
-	
-	
-	
-	do{
-		menu.show();
-		cin>>choice;
-		switch(choice){
-			case 1:
-				passwordManager.viewPasswords();
-				break;
-			case 2:
-				passwordManager.createNewPassword();
-				break;
-			case 3:
-				passwordManager.generatePassword();
-				break;
-			case 4:
-				passwordManager.editPassword();
-				break;
-			case 5:
-				passwordManager.deletePassword();
-				break;				
-		}
-	}while(choice !=6);
-
-	
-	
-	return 0;
+    srand(static_cast<unsigned>(time(nullptr)));
+    int choice;
+    do {
+        showMenu();
+        choice = getChoice(1, 6);
+        switch (choice) {
+            case 1: viewPasswords();    break;
+            case 2: addPassword();     break;
+            case 3: addPassword();     break;  // Generate is inside addPassword
+            case 4: editPassword();    break;
+            case 5: deletePassword();  break;
+            case 6: clearScreen(); cout << "Goodbye!\n"; break;
+        }
+        if (choice != 6) {
+            cout << "Press Enter to continue...";
+            cin.get();
+        }
+    } while (choice != 6);
+    return 0;
 }
